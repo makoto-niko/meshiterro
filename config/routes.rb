054678dd2f1ee6209d1d4e92085676fe704d742c
makoto-niko <mmana19970613@gmail.com>
2024-02-12 09:10:21 +0000

    [finish]4-17

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'user/show'
-  get 'user/edit'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
 resources :post_images, only: [:new, :create, :index, :show, :destroy]
 get 'homes/about', to: 'homes#about', as: 'about'
-resources :users, only: [:show, :edit]
+resources :users, only: [:show, :edit, :update]
 end
